bae0d33f99cadedeaca79c66d200112fd42dbcba
otacrazy2123 <andre.sousa.tec@gmail.com>
2021-02-02 09:32:09 -0300

    Initial commit

---
+++ src/test/java/br/com/desafiozup/tentativaum/TentativaumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiozup.tentativaum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentativaumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
